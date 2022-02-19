0905bd47fcc009740a3da56c27a3e7ae45f2cfb6
Nathan Hüsken <nathan@wintercloud.de>
2022-02-19 19:39:52 +0100

    [fix] in the brightness test the result should be checked

--- examples/set_brightness.rs
+++ examples/set_brightness.rs
@@ -4,5 +4,5 @@ fn main() {
     let hidapi = hidapi::HidApi::new().unwrap();
     let device = StreamDeckDevice::open_first_device(&hidapi).unwrap();
     // make it completely dark!
-    device.set_brightness(0);
+    device.set_brightness(0).unwrap();
 }
